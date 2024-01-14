ecba1042ca4982359f7fbd6c960bf7246d761a00
CarlaMarti <carla.marti3@alumni.esade.edu>
2024-01-14 19:43:09 +0100

    Command Line Options
    Defined the options

--- scripts/homework3.py
+++ scripts/homework3.py
@@ -25,6 +25,18 @@ import os
 import click
 import pandas as pd
 
+@click.command(short_help='Parser to manage inputs for BooksDataset')#info
+@click.option('-id','--input_data', required=True, help='Path to my input dataset')
+@click.option('-o','--output', default="outputs", help="Folder to save all outputs")
+@click.option('-f','--filtering', is_flag=True, help="Set a filtering or not")
+@click.option("-g", "--genre", help="Set a genre (you have to write the genre like this: Action)", required=True)
+@click.option("-y", "--year", help="Set a year (you have to write it like this: 2002)", required=True)
+
+#python scripts/homework3.py -id FilmGenreStats.csv
+
+
+#python scripts/homework3.py -id FilmGenreStats.csv -o Results -f -g Action -y 2002
+
 def main():
     """
     Deal with the input data and send to other functions, in this case inside the class filter_data.
