b37c58176d01d07dfabc5ed1365cf2f0ce7cb089
CarlaMarti <carla.marti3@alumni.esade.edu>
2024-01-14 19:21:40 +0100

    Script Structure
    Defined the task to realize and structured the script (4 parts).

---
+++ scripts/homework3.py
@@ -0,0 +1,37 @@
+# HOMEWORK:
+
+# Download dataset FilmGenreStats from the Moodle
+
+# Let’s create a script within our folder that is connected to Github. This script should have:
+
+        # - Classes
+        # - Click
+        # - Try & Except
+        # - Maybe some import pdb;pdb.set_trace()
+        
+# Every meaningful step we take should correspond with a commit
+
+"""
+Script of practice 2 of making updates in GitHub
+"""
+
+class filter_data:
+    """
+    Class to filter data by year and genre.
+    """
+
+
+import os 
+import click
+import pandas as pd
+
+def main():
+    """
+    Deal with the input data and send to other functions, in this case inside the class filter_data.
+    """
+
+
+
+if __name__ == '__main__':
+    print("\n\n\nTHIS IS WORKING!!\n\n\n")
+    main()
